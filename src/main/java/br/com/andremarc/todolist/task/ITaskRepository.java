369db8957b63be04431c9634524e5fbe1b53d1cc
André Marc <andreluis0015@gmail.com>
2023-10-13 00:06:44 -0300

    add: aula 4

--- src/main/java/br/com/andremarc/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/andremarc/todolist/task/ITaskRepository.java
@@ -3,7 +3,9 @@ package br.com.andremarc.todolist.task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID>, JpaSpecificationExecutor<TaskModel> {
+    List<TaskModel> findByIdUser(UUID idUser);
 }
