a9b231f93a59c4691f8b0d4e2ab98dd1995aef5c
André <andre.marcelino@parnamirim.rn.gov.br>
2023-10-31 08:44:15 -0300

    add: dockerfile e fim do projeto

--- src/main/java/br/com/andremarc/todolist/task/TaskModel.java
+++ src/main/java/br/com/andremarc/todolist/task/TaskModel.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
+import java.rmi.server.ExportException;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -28,4 +29,11 @@ public class TaskModel {
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception {
+        if (title.length() > 50) {
+            throw new Exception("O campo title deve conter no máximo 50 caracteres");
+        }
+        this.title = title;
+    }
 }
